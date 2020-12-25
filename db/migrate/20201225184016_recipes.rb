802a4830f9cf10d4bb7ce1d9fdb781ae95c157ed
Christian Simon <csimon1127@gmail.com>
2020-12-25 18:43:54 +0000

    Automatically backed up by Learn

--- db/migrate/20201225184016_recipes.rb
+++ db/migrate/20201225184016_recipes.rb
@@ -1,5 +1,9 @@
 class Recipes < ActiveRecord::Migration
   def change
-    
+    create_table :recipes do |t|
+      t.string = :name 
+      t.string = ingredients
+      t.float
+    end
   end
 end
